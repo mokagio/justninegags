cf690569a7830c6cc4c5a3e7a35034824ea278e8
Giovanni Lodi <ing.giovanni.lodi@gmail.com>
2013-01-06 11:31:16 +0000

    Added pod for SMWebRequest

--- JustNineGags/ViewController.h
+++ JustNineGags/ViewController.h
@@ -7,7 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "SMWebRequest.h"
+#import <SMWebRequest.h>
 
 @interface ViewController : UIViewController <SMWebRequestDelegate>
 
--- Podfile
+++ Podfile
@@ -1,3 +1,4 @@
 platform :ios
 pod 'MBProgressHUD', '~> 0.5'
 pod 'Reachability',  '~> 3.1.0'
+pod 'SMWebRequest', '~> 1.0'
